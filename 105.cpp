a08433eef933763ded64c266a27684be14c8a718
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2016-01-08 11:38:00 +0330

    Create 105.cpp

---
+++ 105.cpp
@@ -0,0 +1,16 @@
+//Bismillah
+#include <bits/stdc++.h>
+using namespace std; 
+#define FOR(i,begin,end) for (ll i=begin;i<=end;i++)
+#define rep(i,t) for (ll i=0;i<t;i++)
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    ll n;
+    cin >> n;
+    cout << (2*n)/3 << endl;
+    return 0;
+}
